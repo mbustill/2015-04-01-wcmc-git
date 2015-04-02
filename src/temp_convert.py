e790198613e5e6bd5212355ad3541ba8c36b57ef
Vidur Garg <vig2013@med.cornell.edu>
2015-04-02 10:10:35 -0400

    Adding F_to_K

--- src/temp_convert.py
+++ src/temp_convert.py
@@ -1,5 +1,10 @@
 def F_to_K(temp):
 	return ((temp-32)*(5/float(9))) + 273.15
 
-def K-to_C(temp):
+def K_to_C(temp):
 	return (temp)-273.5
+
+def F_to_K(temp):
+	temp_k = F_to_K(temp)
+	result = K_to_C(temp)
+	return(result)
